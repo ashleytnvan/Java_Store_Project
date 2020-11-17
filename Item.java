c1726768d54a83a87283da708e1aeeb3c64a38f1
ashleytnvan <57467096+ashleytnvan@users.noreply.github.com>
2020-11-16 21:12:30 -0800

    Update Item.java
    NOT FINISHED

--- Item.java
+++ Item.java
@@ -1,19 +1,19 @@
 package milestone4;
 
-import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
-public class Item extends JPanel {
+public class Item extends JPanel{
     private String item_name;
     private String picture;
     private double price;
     private double discounted_price;
-
+    
     JLabel pictureLabel;
     JLabel costLabel;
     JLabel discountLabel;
     JLabel nameLabel;
-    JPanel jp;
 
     public Item(String item_name, String picture, double price, double discounted_price){
         this.item_name = item_name;
@@ -21,53 +21,26 @@ public class Item extends JPanel {
         this.price = price;
         this.discounted_price = discounted_price;
 
-        pictureLabel = new JLabel();
-        updatePicture();
-        add(pictureLabel, BorderLayout.CENTER);
-        /*
         nameLabel = new JLabel(getItemName());
+    	pictureLabel = new JLabel();
+    	
+    	costLabel = new JLabel();
+    	float total = (float)getPrice();
+    	String s = Float.toString(total);
+    	costLabel.setText(s);
+    	
 
-        float total = (float)getPrice();
-
-        costLabel = new JLabel();
-        String s = Float.toString(total);
-        costLabel.setText(s);
-
-        float total2 = (float)getDiscount();
-        discountLabel = new JLabel();
-        String s2 = Float.toString(total2);
-        discountLabel.setText(s2);
-
-        add(nameLabel, BorderLayout.CENTER);
-
+    	discountLabel = new JLabel();
+    	float total2 = (float)getDiscount();
+    	String s2 = Float.toString(total2);
+    	discountLabel.setText(s2);
+    	
+        updatePicture();
+        add(nameLabel,BorderLayout.CENTER);
+        add(pictureLabel, BorderLayout.CENTER);
         add(costLabel, BorderLayout.CENTER);
         add(discountLabel, BorderLayout.CENTER);
-        */
-    }
-
-    protected void updatePicture() {
-        //Get the icon corresponding to the image.
-        ImageIcon icon = createImageIcon("images/"
-                + getItemName() + ".gif");
-        pictureLabel.setIcon(icon);
-        pictureLabel.setToolTipText(getItemName().toString());
-
-        if (icon == null) {
-            pictureLabel.setText("Missing Image");
-        } else {
-            pictureLabel.setText(null);
-        }
-        pictureLabel.setPreferredSize(new Dimension(225,225));
-    }
-
-    protected static ImageIcon createImageIcon(String path) {
-        java.net.URL imgURL = Item.class.getResource(path);
-        if (imgURL != null) {
-            return new ImageIcon(imgURL);
-        } else {
-            System.err.println("Couldn't find file: " + path);
-            return null;
-        }
+        
     }
 
     public Item getItem(){
@@ -94,13 +67,6 @@ public class Item extends JPanel {
         return (discounted_price != 0);
     }
 
-    public boolean hasPicture(){
-        if (picture != null)
-            return true;
-        else
-            return false;
-    }
-
     public void setDiscount(int discounted_price){
         this.discounted_price = discounted_price;
     }
@@ -121,5 +87,58 @@ public class Item extends JPanel {
         return "Item[name="+item_name+",picture="+picture+
                 ",price="+price+",discounted_price="+
                 discounted_price+"]";
+    }
+    
+    protected void updatePicture() {
+        //Get the icon corresponding to the image.
+        ImageIcon icon = createImageIcon("images/"
+        		+ getItemName() + ".gif");
+        if (icon == null) {
+            pictureLabel.setText("Missing Image");
+        } else {
+            pictureLabel.setText(null);
+        }
+        //resizing the icon
+        Image img = icon.getImage();
+        Image newImg = img.getScaledInstance(40, 40, Image.SCALE_SMOOTH);
+        ImageIcon image = new ImageIcon(newImg);
+        pictureLabel.setIcon(image);
+        pictureLabel.setToolTipText(getItemName().toString());
+        
+        
+    }
+    
+    protected static ImageIcon createImageIcon(String path) {
+        java.net.URL imgURL = Item.class.getClassLoader().getResource(path);
+        if (imgURL != null) {
+        	System.out.println(imgURL);
+            return new ImageIcon(imgURL);
+        } else {
+            System.err.println("Couldn't find file: " + path);
+            return null;
+        }
+    }
+    private static void createAndShowGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("Items");
+        //Display the window.
+        frame.setSize(400,300);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Create and set up the content pane.
+        JComponent newContentPane = new Item("banana","banana",1.50,1.50);
+        newContentPane.setOpaque(true); //content panes must be opaque
+        frame.setContentPane(newContentPane);
+        
+    }
+    public static void main(String[] args) {
+    	//Schedule a job for the event-dispatching thread:
+        //creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
     }
 }
