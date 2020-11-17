550bd3aa28628f4b5c9eaf33e3e548b8639d8465
Paul Zheng <paul.zheng@sjsu.edu>
2020-11-17 07:33:35 -0800

    updated checkout

--- Tester.java
+++ Tester.java
@@ -5,6 +5,7 @@ public class Tester {
     public static void main(String[] args) {
         //ShoppingApp shop = new ShoppingApp();
         //Invoice invoice = new Invoice();
-        ShoppingCart cart = new ShoppingCart();
+        //ShoppingCart cart = new ShoppingCart();
+        Checkout checkout = new Checkout();
     }
 }
