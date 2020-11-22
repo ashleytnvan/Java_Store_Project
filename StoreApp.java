f71263ff7ec3642f0342e146f04be4a4ccad61d5
jcal329 <50634744+jcal329@users.noreply.github.com>
2020-11-21 20:10:33 -0800

    Add files via upload
    added the skeleton for the GUI and added an image for shopping cart button

--- StoreApp.java
+++ StoreApp.java
@@ -1,5 +1,4 @@
 package milestone4;
-
 import java.util.ArrayList;
 import java.awt.*;
 import java.awt.event.*;
@@ -7,19 +6,56 @@ import javax.swing.*;
 
 public class StoreApp {
     private ArrayList<Item> items;
+    private ShoppingCart cart;
 
+    JFrame frame = new JFrame("Store");
+    JPanel base = new JPanel();
+    JPanel header = new JPanel();
+    JScrollPane itemDisplay = new JScrollPane();
 
-    public StoreApp(){
+    JButton exitButton = new JButton("Cancel");
+    JButton cartButton = new JButton(new ImageIcon("/images/shopping_cart.png"));
 
+    public StoreApp(ShoppingCart cart){
+        // initialize data structures
         items = new ArrayList<Item>();
+        this.cart = cart;
+
+        // base panel
+        base.setLayout(new BorderLayout()); // use base border layout
+
+        // header panel
+        header.setLayout(new FlowLayout(FlowLayout.RIGHT)); // right justified
+        cartButton.addActionListener(event -> cart.showCart());
+        exitButton.addActionListener(event -> exit());
+        header.add(cartButton);
+        header.add(exitButton);
+        base.add(header, BorderLayout.NORTH);
+
+        // item display
+        itemDisplay.setLayout(new ScrollPaneLayout());
+        for(Item i: items) {
+            itemDisplay.add(i);
+        }
+        base.add(itemDisplay, BorderLayout.CENTER);
+
+        // frame visibility
+        frame.add(base);
+        frame.setSize(500, 500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
     }
 
-    public void startNew(){
+    public void startNew() {
 
     }
 
     public void exit(){
 
+        frame.setVisible(false);
+        frame.dispose();
+        cart.cancel();
     }
 
     public String listItems(){
@@ -39,11 +75,11 @@ public class StoreApp {
         items.remove(items.indexOf(item));
     }
 
-    public void clear(){
-
+    public void clear() {
+        this.items = new ArrayList<>();
     }
 
-    public int size(){
+    public int stock() {
         return items.size();
     }
 }
